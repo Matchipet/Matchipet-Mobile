e8d70b4f20a9fd7869268da9239843c183f70fd5
DiegoTalledoS18 <85322967+DiegoTalledoS18@users.noreply.github.com>
2023-07-23 01:30:59 -0500

    profile preferences

--- lib/pages/adopter/profile_page.dart
+++ lib/pages/adopter/profile_page.dart
@@ -8,6 +8,8 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  List petPreferences=[true,true,false,false,false,false];
+  List petPreferencesName=["Perros","Gatos","Conejos","Aves","Peces","Hamsters"];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -84,10 +86,10 @@ class _ProfilePageState extends State<ProfilePage> {
                 ],
               ),
               SizedBox(height: 20),
-// Divider
+              // Divider
               Divider(thickness: 2),
               SizedBox(height: 20),
-// Container con "Ahora mismo estoy buscando"
+              // Container con "Ahora mismo estoy buscando"
               Container(
                 padding: EdgeInsets.all(8),
                 child: Text(
@@ -97,20 +99,77 @@ class _ProfilePageState extends State<ProfilePage> {
               ),
               SizedBox(height: 10),
               Container(
-                height: 200, // Establece la altura deseada
+                height: 200,
                 child: GridView.builder(
                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                     crossAxisCount: 3,
                     childAspectRatio: (MediaQuery.of(context).size.width / 3) / 100,
-                  ),
-                  physics: NeverScrollableScrollPhysics(),
-                  itemCount: 6,
-                  itemBuilder: (context, index) {
-                    return Icon(Icons.pets, size: 30);
-                  },
                 ),
-              ),
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: petPreferences.length,
+                itemBuilder: (context, index) {
+                  String imageName;
+                  if (index == 0)
+                    imageName = 'dog_icon.png';
+                  else if (index == 1)
+                    imageName = 'cat_icon.png';
+                  else if (index == 2)
+                    imageName = 'bunny_icon.png';
+                  else if (index == 3)
+                    imageName = 'bird_icon.png';
+                  else if (index == 4)
+                    imageName = 'fish_icon.png';
+                  else if (index == 5)
+                    imageName = 'hamster_icon.png';
+                  else
+                    imageName = 'default_icon.png'; // Valor predeterminado en caso de que no coincida con ningún índice
 
+                  return GestureDetector(
+                    onTap: () {
+                    setState(() {
+                      petPreferences[index] = !petPreferences[index];
+                    });
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Material(
+                        elevation: 4, // Configura la elevación para agregar sombra
+                        shadowColor: Colors.black.withOpacity(0.4), // Ajusta la opacidad para hacer la sombra más tenue
+                        borderRadius: BorderRadius.circular(10),
+                        color: petPreferences[index] ? Colors.red : Colors.white,
+                        child: Column( // Usamos una columna para tener el Container y el Text debajo
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              width: 50,
+                              height: 50,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                border: Border.all(color: petPreferences[index] ? Colors.red : Colors.white, width: 2),
+                              ),
+                              child: Container(
+                                padding: const EdgeInsets.all(1),
+                                child: Image.asset(
+                                  'lib/images/$imageName',
+                                  color: petPreferences[index] ? Colors.white : Colors.black,
+                                  width: 20,
+                                  height: 20,
+                                ),
+                              ),
+                            ),
+                            Text(
+                              petPreferencesName[index], // Llamamos a una función que obtiene el nombre del índice
+                              style: TextStyle(fontSize: 12, color: petPreferences[index] ? Colors.white : Colors.black),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+
+                  );
+              },
+            ),
+          ),
             ],
           ),
         ),
